342ebbd7d818394af624b9e7f953d78ec8d9f11f
rasbt <mail@sebastianraschka.com>
2024-02-11 11:51:39 -0600

    ch4 exercise solutions

--- ch04/01_main-chapter-code/gpt.py
+++ ch04/01_main-chapter-code/gpt.py
@@ -187,12 +187,11 @@ class GPTModel(nn.Module):
         super().__init__()
         self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
         self.pos_emb = nn.Embedding(cfg["ctx_len"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
 
-        # Use a placeholder for TransformerBlock
         self.trf_blocks = nn.Sequential(
             *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
 
-        # Use a placeholder for LayerNorm
         self.final_norm = LayerNorm(cfg["emb_dim"])
         self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
 

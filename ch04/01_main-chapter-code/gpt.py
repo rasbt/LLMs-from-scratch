559304f8a4d588702bb08eb649d1e48d8d0321b4
Rayed Bin Wahed <rayed@therapservices.net>
2024-03-04 12:15:18 +0800

    Update gpt.py
    Delete unused dropout layer

--- ch04/01_main-chapter-code/gpt.py
+++ ch04/01_main-chapter-code/gpt.py
@@ -189,7 +189,6 @@ class GPTModel(nn.Module):
         super().__init__()
         self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
         self.pos_emb = nn.Embedding(cfg["ctx_len"], cfg["emb_dim"])
-        self.drop_emb = nn.Dropout(cfg["drop_rate"])
 
         self.trf_blocks = nn.Sequential(
             *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])

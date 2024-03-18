ca96abac8aa82a29e6ad61fece6258954b11f9e5
Sebastian Raschka <mail@sebastianraschka.com>
2024-03-18 11:58:37 -0500

    Set up basic test gh worklows (#79)
    * Set up basic test gh worklows
    
    * update file paths
    
    * env check
    
    * add env check
    
    * Update requirements.txt
    
    * simplify
    
    * upd

---
+++ ch04/01_main-chapter-code/tests.py
@@ -0,0 +1,33 @@
+from gpt import main
+
+expected = """
+==================================================
+                      IN
+==================================================
+
+Input text: Hello, I am
+Encoded input text: [15496, 11, 314, 716]
+encoded_tensor.shape: torch.Size([1, 4])
+
+
+==================================================
+                      OUT
+==================================================
+
+Output: tensor([[15496,    11,   314,   716, 27018, 24086, 47843, 30961, 42348,  7267,
+         49706, 43231, 47062, 34657]])
+Output length: 14
+Output text: Hello, I am Featureiman Byeswickattribute argue logger Normandy Compton analogous
+"""
+
+
+def test_main(capsys):
+    main()
+    captured = capsys.readouterr()
+
+    # Normalize line endings and strip trailing whitespace from each line
+    normalized_expected = '\n'.join(line.rstrip() for line in expected.splitlines())
+    normalized_output = '\n'.join(line.rstrip() for line in captured.out.splitlines())
+
+    # Compare normalized strings
+    assert normalized_output == normalized_expected

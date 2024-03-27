88b2dd780a2e647907f2595f443c6703fcf17b71
rasbt <mail@sebastianraschka.com>
2024-03-27 07:11:56 -0500

    make batch loss calculatution more efficient

--- ch05/03_bonus_pretraining_on_gutenberg/previous_chapters.py
+++ ch05/03_bonus_pretraining_on_gutenberg/previous_chapters.py
@@ -253,6 +253,8 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
     total_loss = 0.
     if num_batches is None:
         num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
     for i, (input_batch, target_batch) in enumerate(data_loader):
         if i < num_batches:
             loss = calc_loss_batch(input_batch, target_batch, model, device)

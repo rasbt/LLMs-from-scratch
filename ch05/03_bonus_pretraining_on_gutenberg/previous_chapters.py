1dd0f0ccdcd095eedf44884dd54281bc672933d0
rasbt <mail@sebastianraschka.com>
2024-04-04 20:46:02 -0500

    Fix Loss in Gutenberg bonus section

--- ch05/03_bonus_pretraining_on_gutenberg/previous_chapters.py
+++ ch05/03_bonus_pretraining_on_gutenberg/previous_chapters.py
@@ -244,7 +244,7 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
 def calc_loss_batch(input_batch, target_batch, model, device):
     input_batch, target_batch = input_batch.to(device), target_batch.to(device)
     logits = model(input_batch)
-    loss = torch.nn.functional.cross_entropy(logits.flatten(0, -1), target_batch.flatten())
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
     return loss
 
 

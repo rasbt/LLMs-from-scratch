52f15dff303d7dc1b53042e3e4262252faf83679
rasbt <mail@sebastianraschka.com>
2024-05-14 08:27:46 -0500

    fix file path name

--- ch06/03_bonus_imdb-classification/train-sklearn-logreg.py
+++ ch06/03_bonus_imdb-classification/train-sklearn-logreg.py
@@ -13,7 +13,7 @@ from sklearn.dummy import DummyClassifier
 
 def load_dataframes():
     df_train = pd.read_csv("train.csv")
-    df_val = pd.read_csv("val.csv")
+    df_val = pd.read_csv("validation.csv")
     df_test = pd.read_csv("test.csv")
 
     return df_train, df_val, df_test

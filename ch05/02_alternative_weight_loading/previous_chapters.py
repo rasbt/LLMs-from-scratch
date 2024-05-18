4851d5a0fabe615a1a94bc1b2f9d4ab5fea02ace
rasbt <mail@sebastianraschka.com>
2024-05-18 12:35:40 -0500

    add eos_id option for ch07

--- ch05/02_alternative_weight_loading/previous_chapters.py
+++ ch05/02_alternative_weight_loading/previous_chapters.py
@@ -254,7 +254,7 @@ def token_ids_to_text(token_ids, tokenizer):
     return tokenizer.decode(flat.tolist())
 
 
-def generate(model, idx, max_new_tokens, context_size, temperature, top_k=None):
+def generate(model, idx, max_new_tokens, context_size, temperature, top_k=None, eos_id=None):
 
     # For-loop is the same as before: Get logits, and only focus on last time step
     for _ in range(max_new_tokens):
@@ -283,6 +283,9 @@ def generate(model, idx, max_new_tokens, context_size, temperature, top_k=None):
         # Otherwise same as before: get idx of the vocab entry with the highest logits value
         else:
             idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch_size, 1)
+
+        if idx_next == eos_id:  # Stop generating early if end-of-sequence token is encountered and eos_id is specified
+            break
 
         # Same as before: append sampled index to the running sequence
         idx = torch.cat((idx, idx_next), dim=1)  # (batch_size, num_tokens+1)

915ef43aff45d3edfb688f6538a529f85564fd4d
Daniel Kleine <53251018+d-kleine@users.noreply.github.com>
2024-06-19 13:59:05 +0000

    fixed num_workers

--- ch05/03_bonus_pretraining_on_gutenberg/previous_chapters.py
+++ ch05/03_bonus_pretraining_on_gutenberg/previous_chapters.py
@@ -44,7 +44,7 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,
     tokenizer = tiktoken.get_encoding("gpt2")
     dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
     dataloader = DataLoader(
-        dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=0)
+        dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
 
     return dataloader
 

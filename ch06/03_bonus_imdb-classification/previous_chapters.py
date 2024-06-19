5449ed08317c8c29272cedafef2296b2af9e6ebb
Daniel Kleine <53251018+d-kleine@users.noreply.github.com>
2024-06-19 15:24:07 +0000

    ch06 & ch07: added num_workers to create_dataloader_v1

--- ch06/03_bonus_imdb-classification/previous_chapters.py
+++ ch06/03_bonus_imdb-classification/previous_chapters.py
@@ -42,7 +42,7 @@ class GPTDatasetV1(Dataset):
 
 
 def create_dataloader_v1(txt, batch_size=4, max_length=256,
-                         stride=128, shuffle=True, drop_last=True):
+                         stride=128, shuffle=True, drop_last=True, num_workers=0):
     # Initialize the tokenizer
     tokenizer = tiktoken.get_encoding("gpt2")
 
@@ -51,7 +51,7 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,
 
     # Create dataloader
     dataloader = DataLoader(
-        dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
+        dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
 
     return dataloader
 

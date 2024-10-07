42f3f80e20fd240ffcd76851e43352710d66d96e
rasbt <mail@sebastianraschka.com>
2024-10-07 08:12:47 -0500

    update

--- ch05/07_gpt_to_llama/tests/tests.py
+++ ch05/07_gpt_to_llama/tests/tests.py
@@ -18,39 +18,45 @@ from transformers.models.llama.modeling_llama import LlamaRotaryEmbedding, apply
 
 @pytest.fixture(scope="module")
 def notebook():
-    def import_definitions_from_notebook(fullname, names):
-        # Get the directory of the current test file
-        current_dir = os.path.dirname(__file__)
-        path = os.path.join(current_dir, "..", fullname + ".ipynb")
-        path = os.path.normpath(path)
+    def import_definitions_from_notebook(notebooks):
+        imported_modules = {}
 
-        # Load the notebook
-        if not os.path.exists(path):
-            raise FileNotFoundError(f"Notebook file not found at: {path}")
+        for fullname, names in notebooks.items():
+            # Get the directory of the current test file
+            current_dir = os.path.dirname(__file__)
+            path = os.path.join(current_dir, "..", fullname + ".ipynb")
+            path = os.path.normpath(path)
 
-        with io.open(path, "r", encoding="utf-8") as f:
-            nb = nbformat.read(f, as_version=4)
+            # Load the notebook
+            if not os.path.exists(path):
+                raise FileNotFoundError(f"Notebook file not found at: {path}")
 
-        # Create a module to store the imported functions and classes
-        mod = types.ModuleType(fullname)
-        sys.modules[fullname] = mod
+            with io.open(path, "r", encoding="utf-8") as f:
+                nb = nbformat.read(f, as_version=4)
 
-        # Go through the notebook cells and only execute function or class definitions
-        for cell in nb.cells:
-            if cell.cell_type == "code":
-                cell_code = cell.source
-                for name in names:
-                    # Check for function or class definitions
-                    if f"def {name}" in cell_code or f"class {name}" in cell_code:
-                        exec(cell_code, mod.__dict__)
-        return mod
+            # Create a module to store the imported functions and classes
+            mod = types.ModuleType(fullname)
+            sys.modules[fullname] = mod
 
-    # Specify the notebook name and functions/classes to import
-    fullname = "converting-gpt-to-llama2"
-    names = ["precompute_rope_params", "compute_rope", "SiLU", "RMSNorm"]
+            # Go through the notebook cells and only execute function or class definitions
+            for cell in nb.cells:
+                if cell.cell_type == "code":
+                    cell_code = cell.source
+                    for name in names:
+                        # Check for function or class definitions
+                        if f"def {name}" in cell_code or f"class {name}" in cell_code:
+                            exec(cell_code, mod.__dict__)
 
-    # Import the required functions and classes from the notebook
-    return import_definitions_from_notebook(fullname, names)
+            imported_modules[fullname] = mod
+
+        return imported_modules
+
+    notebooks = {
+        "converting-gpt-to-llama2": ["SiLU", "RMSNorm", "precompute_rope_params", "compute_rope"],
+        "converting-llama2-to-llama3": ["precompute_rope_params"]
+    }
+
+    return import_definitions_from_notebook(notebooks)
 
 
 @pytest.fixture(autouse=True)
@@ -59,6 +65,9 @@ def set_seed():
 
 
 def test_rope_llama2(notebook):
+
+    this_nb = notebook["converting-gpt-to-llama2"]
+
     # Settings
     batch_size = 1
     context_len = 4096
@@ -66,15 +75,15 @@ def test_rope_llama2(notebook):
     head_dim = 16
 
     # Instantiate RoPE parameters
-    cos, sin = notebook.precompute_rope_params(head_dim=head_dim, context_length=context_len)
+    cos, sin = this_nb.precompute_rope_params(head_dim=head_dim, context_length=context_len)
 
     # Dummy query and key tensors
     queries = torch.randn(batch_size, num_heads, context_len, head_dim)
     keys = torch.randn(batch_size, num_heads, context_len, head_dim)
 
     # Apply rotary position embeddings
-    queries_rot = notebook.compute_rope(queries, cos, sin)
-    keys_rot = notebook.compute_rope(keys, cos, sin)
+    queries_rot = this_nb.compute_rope(queries, cos, sin)
+    keys_rot = this_nb.compute_rope(keys, cos, sin)
 
     rot_emb = LlamaRotaryEmbedding(
         dim=head_dim,
@@ -93,6 +102,10 @@ def test_rope_llama2(notebook):
 
 
 def test_rope_llama3(notebook):
+
+    nb1 = notebook["converting-gpt-to-llama2"]
+    nb2 = notebook["converting-llama2-to-llama3"]
+
     # Settings
     batch_size = 1
     context_len = 8192
@@ -101,7 +114,7 @@ def test_rope_llama3(notebook):
     theta_base = 50_000
 
     # Instantiate RoPE parameters
-    cos, sin = notebook.precompute_rope_params(
+    cos, sin = nb2.precompute_rope_params(
         head_dim=head_dim,
         context_length=context_len,
         theta_base=theta_base
@@ -113,8 +126,8 @@ def test_rope_llama3(notebook):
     keys = torch.randn(batch_size, num_heads, context_len, head_dim)
 
     # Apply rotary position embeddings
-    queries_rot = notebook.compute_rope(queries, cos, sin)
-    keys_rot = notebook.compute_rope(keys, cos, sin)
+    queries_rot = nb1.compute_rope(queries, cos, sin)
+    keys_rot = nb1.compute_rope(keys, cos, sin)
 
     rot_emb = LlamaRotaryEmbedding(
         dim=head_dim,
@@ -132,7 +145,11 @@ def test_rope_llama3(notebook):
     torch.testing.assert_close(queries_rot, ref_queries_rot)
 
 
-def test_rope_llama3_12():
+def test_rope_llama3_12(notebook):
+
+    nb1 = notebook["converting-gpt-to-llama2"]
+    nb2 = notebook["converting-llama2-to-llama3"]
+
     # Settings
     batch_size = 1
     context_len = 8192
@@ -148,11 +165,11 @@ def test_rope_llama3_12():
     }
 
     # Instantiate RoPE parameters
-    cos, sin = notebook.compute_rope(
+    cos, sin = nb2.precompute_rope_params(
         head_dim=head_dim,
-        base=rope_theta,
+        theta_base=rope_theta,
         context_length=context_len,
-        config=rope_config,
+        freq_config=rope_config,
     )
 
     # Dummy query and key tensors
@@ -161,8 +178,8 @@ def test_rope_llama3_12():
     keys = torch.randn(batch_size, num_heads, context_len, head_dim)
 
     # Apply rotary position embeddings
-    queries_rot = notebook.compute_rope(queries, cos, sin)
-    keys_rot = notebook.compute_rope(keys, cos, sin)
+    queries_rot = nb1.compute_rope(queries, cos, sin)
+    keys_rot = nb1.compute_rope(keys, cos, sin)
 
     hf_rope_params = {
         "factor": 8.0,
@@ -197,14 +214,14 @@ def test_rope_llama3_12():
 
 def test_silu(notebook):
     example_batch = torch.randn(2, 3, 4)
-    silu = notebook.SiLU()
+    silu = notebook["converting-gpt-to-llama2"].SiLU()
     assert torch.allclose(silu(example_batch), torch.nn.functional.silu(example_batch))
 
 
 @pytest.mark.skipif(torch.__version__ < "2.4", reason="Requires PyTorch 2.4 or newer")
 def test_rmsnorm(notebook):
     example_batch = torch.randn(2, 3, 4)
-    rms_norm = notebook.RMSNorm(emb_dim=example_batch.shape[-1], eps=1e-5)
+    rms_norm = notebook["converting-gpt-to-llama2"].RMSNorm(emb_dim=example_batch.shape[-1], eps=1e-5)
     rmsnorm_pytorch = torch.nn.RMSNorm(example_batch.shape[-1], eps=1e-5)
 
     assert torch.allclose(rms_norm(example_batch), rmsnorm_pytorch(example_batch))

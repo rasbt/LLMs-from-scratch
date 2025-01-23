61e05dbca19029e2a54a0ed2ae57188b091a9522
rasbt <mail@sebastianraschka.com>
2025-01-23 09:20:57 -0600

    A few cosmetic updates

--- ch07/01_main-chapter-code/gpt_instruction_finetuning.py
+++ ch07/01_main-chapter-code/gpt_instruction_finetuning.py
@@ -103,9 +103,6 @@ def download_and_load_file(file_path, url):
             text_data = response.read().decode("utf-8")
         with open(file_path, "w", encoding="utf-8") as file:
             file.write(text_data)
-    else:
-        with open(file_path, "r", encoding="utf-8") as file:
-            text_data = file.read()
 
     with open(file_path, "r") as file:
         data = json.load(file)

8d5cb413339e7c951f2cb148e2c664472bab3a5e
rasbt <mail@sebastianraschka.com>
2025-06-16 14:49:37 -0500

    interpretable generate

--- ch04/03_kv-cache/gpt_with_kv_cache.py
+++ ch04/03_kv-cache/gpt_with_kv_cache.py
@@ -267,11 +267,12 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
 def generate_text_simple_cached(model, idx, max_new_tokens, use_cache=True):
     model.eval()
 
+    ctx_len = model.pos_emb.num_embeddings  # max supported length, e.g. 1024
     if use_cache:
         # Init cache with full prompt
         model.reset_kv_cache()
         with torch.no_grad():
-            logits = model(idx, use_cache=True)
+            logits = model(idx[:, -ctx_len:], use_cache=True)
 
         for _ in range(max_new_tokens):
             # a) pick the token with the highest log-probability (greedy sampling)
@@ -282,7 +283,6 @@ def generate_text_simple_cached(model, idx, max_new_tokens, use_cache=True):
             with torch.no_grad():
                 logits = model(next_idx, use_cache=True)
     else:
-        ctx_len = model.pos_emb.num_embeddings  # e.g. 1024
         for _ in range(max_new_tokens):
             with torch.no_grad():
                 logits = model(idx[:, -ctx_len:], use_cache=False)

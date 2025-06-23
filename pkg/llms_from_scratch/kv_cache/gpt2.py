0c335b95413eaa4a6949325f650c56ba620565f3
rasbt <mail@sebastianraschka.com>
2025-06-23 18:00:03 -0500

    cleanup

--- pkg/llms_from_scratch/kv_cache/gpt2.py
+++ pkg/llms_from_scratch/kv_cache/gpt2.py
@@ -3,6 +3,8 @@
 #   - https://www.manning.com/books/build-a-large-language-model-from-scratch
 # Code: https://github.com/rasbt/LLMs-from-scratch
 
+from .utils import KVCache   # noqa: F401
+
 import torch
 import torch.nn as nn
 

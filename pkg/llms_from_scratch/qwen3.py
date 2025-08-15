9f62224eaf5f2a5177f4d0b1f6d64d84c94b9de8
rasbt <mail@sebastianraschka.com>
2025-08-15 08:27:40 -0500

    Fix qk_norm comment

--- pkg/llms_from_scratch/qwen3.py
+++ pkg/llms_from_scratch/qwen3.py
@@ -22,7 +22,7 @@ QWEN_CONFIG_06_B = {
     "n_layers": 28,                  # Number of layers
     "hidden_dim": 3072,              # Size of the intermediate dimension in FeedForward
     "head_dim": 128,                 # Size of the heads in GQA
-    "qk_norm": True,                 # Whether to normalize queries and values in GQA
+    "qk_norm": True,                 # Whether to normalize queries and keys in GQA
     "n_kv_groups": 8,                # Key-Value groups for grouped-query attention
     "rope_base": 1_000_000.0,        # The base in RoPE's "theta"
     "dtype": torch.bfloat16,         # Lower-precision dtype to reduce memory usage
